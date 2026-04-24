7c4cd9ef291f8094515988270e0173829f67e921
v1ktr <sipinvictor@gmail.com>
2026-04-24 14:07:51 +0300

    v1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,9 +14,18 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+    const newPost = postsService.create(post);
+    expect(newPost.id).toBeDefined();
+    expect(newPost.date).toBeDefined();
+    expect(newPost.text).toBe(post.text);    
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    const firstPost = postsService.find('1');
+    expect(firstPost).toBeDefined();
+    expect(firstPost?.id).toBe('1');
+    expect(firstPost?.date).toBeDefined();
+    expect(firstPost?.text).toBe('Some pre-existing post');
   });
 });
