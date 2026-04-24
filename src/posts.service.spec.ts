2e58bb46d8d0cb3541c27df85f9ad9ae9db5c6a7
v1ktr <sipinvictor@gmail.com>
2026-04-24 14:22:17 +0300

    v2

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -15,17 +15,25 @@ describe('PostsService', () => {
   it('should add a new post', () => {
     // реализуйте тест-кейс
     const newPost = postsService.create(post);
-    expect(newPost.id).toBeDefined();
+    expect(newPost.id).toBe('2');
     expect(newPost.date).toBeDefined();
     expect(newPost.text).toBe(post.text);    
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
-    const firstPost = postsService.find('1');
-    expect(firstPost).toBeDefined();
-    expect(firstPost?.id).toBe('1');
-    expect(firstPost?.date).toBeDefined();
-    expect(firstPost?.text).toBe('Some pre-existing post');
+    // const firstPost = postsService.find('1');
+    // expect(firstPost).toBeDefined();
+    // expect(firstPost?.id).toBe('1');
+    // expect(firstPost?.date).toBeDefined();
+    // expect(firstPost?.text).toBe('Some pre-existing post');
+      const newPost = postsService.create(post);
+
+      const found = postsService.find(newPost.id);
+
+      expect(found).toBeDefined();
+      expect(found?.id).toBe(newPost.id);
+      expect(found?.text).toBe(post.text);
+      expect(found?.id).not.toBe('1');
   });
 });
